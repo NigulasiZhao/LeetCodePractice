7e6722ff4fa6796c8bb73b38498f77c5be46e23c
尼古拉斯赵四 <185653517@qq.com>
2020-12-23 14:31:52 +0800

    LeetCode2

---
+++ LeetCodePractice/Controllers/LeetCode2.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Controllers
+{
+    /// <summary>
+    /// 两数相加（给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储一位数字。如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。）
+    /// </summary>
+    public class LeetCode2 : Controller
+    {
+        public string SumNumbers(int[] nums, int target)
+        {
+            var l1 = generateList(new int[] { 1, 5, 7 });
+            var l2 = generateList(new int[] { 9, 8, 2, 9 });
+            printList(l1);
+            printList(l2);
+            var sum = AddTwoNumbers(l1, l2);
+            return printList(sum);
+        }
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int val = 0;
+            ListNode prenode = new ListNode(0);
+            ListNode lastnode = prenode;
+            while (l1 != null || l2 != null || val != 0)
+            {
+                val = val + (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+                lastnode.next = new ListNode(val % 10);
+                lastnode = lastnode.next;
+                val = val / 10;
+                l1 = l1 == null ? null : l1.next;
+                l2 = l2 == null ? null : l2.next;
+            }
+            return prenode.next;
+        }
+        static ListNode generateList(int[] vals)
+        {
+            ListNode res = null;
+            ListNode last = null;
+            foreach (var val in vals)
+            {
+                if (res is null)
+                {
+                    res = new ListNode(val);
+                    last = res;
+                }
+                else
+                {
+                    last.next = new ListNode(val);
+                    last = last.next;
+                }
+            }
+            return res;
+        }
+        static string printList(ListNode l)
+        {
+            string str = string.Empty;
+            while (l != null)
+            {
+                str += l;
+                l = l.next;
+            }
+            return str;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
