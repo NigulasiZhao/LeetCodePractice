3971ee0d463bfc198e4a6aab19c878895999c95d
尼古拉斯赵四 <185653517@qq.com>
2021-01-26 22:58:23 +0800

    已修改        LeetCodePractice/GISServerForCore2.0.xml

---
+++ LeetCodePractice/Controllers/LeetCode20.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Controllers
+{
+    /// <summary>
+    /// 20. 有效的括号
+    /// </summary>
+    public class LeetCode20 : Controller
+    {
+        /// <summary>
+        /// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+        /// 有效字符串需满足：
+        /// 左括号必须用相同类型的右括号闭合。
+        /// 左括号必须以正确的顺序闭合。
+        /// <param name="s"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public bool IsValid(string s)
+        {
+            Stack<char> st = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    st.Push(s[i]);//如果是左括号，入栈
+                }
+                else if (st.Count == 0)//右括号且栈为空，false
+                {
+                    return false;
+                }
+                else if (s[i] == ')' && st.Pop() != '(' || s[i] == '}' && st.Pop() != '{' || s[i] == ']' && st.Pop() != '[')//右括号且栈顶左括号不匹配，false
+                {
+                    return false;
+                }
+            }
+            return st.Count == 0;//如果栈为空，括号全部配对完，返回true
+        }
+    }
+}
